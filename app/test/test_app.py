6d89f604c497a0e22b742e5fa9b809a46a2403fa
zclarkED <zclark@easydynamics.com>
2020-12-18 07:37:42 -0500

    created unit tests for invalid uuid and empty delete request

--- app/test/test_app.py
+++ app/test/test_app.py
@@ -19,3 +19,30 @@ def test_create_transaction_log_function_bad_request_empty_body(client_fixture):
     # empty request
     response = client_fixture.http.post("/transaction")
     assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_create_transaction_log_function_bad_request_invalid_uuid(client_fixture):
+    """ Ensure the create transaction log function returns a 400 Bad Request for invalid uuid """
+    # invalid uuid
+    req_invalid = {
+        "UUID": "c7b82090-172f-11eb-adc1-",
+        "Customer": "Login.gov",
+        "Service": "Attribute Validation",
+        "CSP": "State SSN Verification",
+        "Result": "SSN Match",
+        "Cost": ".25",
+    }
+    response = client_fixture.http.request(
+        method="POST",
+        path="/transaction",
+        headers={"Content-Type": "application/json"},
+        body=json.dumps(req_invalid),
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_delete_transaction_log_function_bad_request_empty_body(client_fixture):
+    """ Ensure the delete transaction log function returns a 400 Bad Request on bad/empty requests """
+    # the UUID path variable is empty
+    response = client_fixture.http.delete("/transaction//3")
+    assert response.status_code == HTTPStatus.BAD_REQUEST
