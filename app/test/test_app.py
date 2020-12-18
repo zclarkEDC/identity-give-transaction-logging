077ef39a16e997c8316789b9bf5357caca52430a
zclarkED <zclark@easydynamics.com>
2020-12-18 06:29:38 -0500

    unit test for empty request body

--- app/test/test_app.py
+++ app/test/test_app.py
@@ -1,21 +1,21 @@
 """ Tests for app.py """
+import json
+from http import HTTPStatus
 from chalice.test import Client
 from pytest import fixture
 from app import app
 
 
-@fixture
+@fixture(name="client_fixture")
 def test_client():
     """ Test fixture for creating a chalice Client """
     with Client(app) as client:
         yield client
 
 
-def test_hello_name_function(test_client):
-    name = "myname"
-    response = test_client.http.get(f"/hello/{name}")
-    assert response.json_body == {"hello": f"{name}"}
-
-    name = "different_name"
-    response = test_client.http.get(f"/hello/{name}")
-    assert response.json_body == {"hello": f"{name}"}
+# transaction log tests
+def test_create_transaction_log_function_bad_request_empty_body(client_fixture):
+    """ Ensure the create transaction log function returns a 400 Bad Request on bad/empty requests """
+    # empty request
+    response = client_fixture.http.post("/transaction")
+    assert response.status_code == HTTPStatus.BAD_REQUEST
